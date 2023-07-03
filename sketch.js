ce383d78f544f449f8318ab35c1859af93f3dcf8
GaganS55 <138513045+GaganS55@users.noreply.github.com>
2023-07-03 11:45:45 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box
+function setup() {
+  createCanvas(2000,1000);
+  box=createSprite(200,200,100,100)
+  console.log(box)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
